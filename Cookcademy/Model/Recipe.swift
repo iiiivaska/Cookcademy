be0677c58c0f95babe168f2c0e878de0fb872c77
iiiivaska <iiiivaska@yandex.ru>
2022-02-17 22:34:25 +0300

    Complete data structure for reciepe model in model/reciepe.swift

--- Cookcademy/Model/Recipe.swift
+++ Cookcademy/Model/Recipe.swift
@@ -8,10 +8,41 @@
 import Foundation
 
 struct Recipe {
+    var mainInformation: MainInformation
+    var ingredients: [Ingredients]
+    var directions: [Direction]
+}
+
+struct MainInformation {
     var name: String
     var description: String
     var author: String
-    var category: String
-    var ingredients: [String]
-    var directions: [String]
+    var category: Category
+    
+    enum Category: String, CaseIterable {
+        case breakfast = "Breakfast"
+        case lunch = "Lunch"
+        case dinner = "Dinner"
+        case dessert = "Dessert"
+    }
+}
+
+struct Ingredients {
+    var name: String
+    var quantity: Double
+    var unit: Unit
+    
+    enum Unit: String, CaseIterable {
+        case oz = "Ounces"
+        case g = "Grams"
+        case cups = "Cups"
+        case tbs = "Tablespoons"
+        case tsp = "Teaspoons"
+        case none = "No units"
+    }
+}
+
+struct Direction {
+    var description: String
+    var isOptional: Bool
 }
