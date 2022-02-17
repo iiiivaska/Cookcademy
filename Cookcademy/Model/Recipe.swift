7720d244611a4c552ee21fefd78008cc26def176
iiiivaska <iiiivaska@yandex.ru>
2022-02-17 22:18:53 +0300

    Initial commit, add skelet of reciepe model

---
+++ Cookcademy/Model/Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Cookcademy
+//
+//  Created by Василий Буланов on 2/17/22.
+//
+
+import Foundation
+
+struct Recipe {
+    var name: String
+    var description: String
+    var author: String
+    var category: String
+    var ingredients: [String]
+    var directions: [String]
+}
