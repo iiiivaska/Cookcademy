9f78fda1388235f3194f4d56124305ac18b67254
iiiivaska <iiiivaska@yandex.ru>
2022-02-17 22:07:16 +0300

    Initial Commit

---
+++ Cookcademy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cookcademy
+//
+//  Created by Василий Буланов on 2/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Василий Буланов on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
